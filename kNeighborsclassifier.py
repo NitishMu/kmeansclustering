6d3b5f8a1852c16a5957c0fd82904115cad5dfba
NitishMu <nitishm366@gmail.com>
2025-03-11 22:35:09 +0530

    Update kNeighborsclassifier.py

--- kNeighborsclassifier.py
+++ kNeighborsclassifier.py
@@ -1,19 +1,13 @@
 from sklearn import datasets
-from sklearn_model_selection import train_test_split
-#
+from sklearn.model_selection import train_test_split
 iris=datasets.load_iris()
 x=iris.data
 y=iris.target
-#
 x_train,x_test,y_train,y_test=train_test_split(x,y,test_size=0.2,random_state=42)
-from sklearn.neighbors import kNeighborsclassifier
-#
-knn=kNeighborsclassifier (n_neighbors=3)
-#
+from sklearn.neighbors import KNeighborsClassifier
+knn=KNeighborsClassifier(n_neighbors=3)
 knn.fit(x_train,y_train)
-#
 y_pred=knn.predict(x_test)
 from sklearn.metrics import accuracy_score
-#
 accuracy=accuracy_score(y_test,y_pred)
-print(f"Accuracy:{accuray}")
+print(f"Accuracy:{accuracy}")
