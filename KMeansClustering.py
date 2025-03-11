3d68a1cf4e11e41db37a0796b8738729ad774ea9
NitishMu <nitishm366@gmail.com>
2025-03-11 22:13:15 +0530

    Update KMeansClustering.py

--- KMeansClustering.py
+++ KMeansClustering.py
@@ -1,23 +1,16 @@
 from sklearn import datasets
-from sklearn.cluster import k means
+from sklearn.cluster import KMeans
 import matplotlib.pyplot as plt
-#
 iris=datasets.load_iris()
-x=iris.data
-#
+X=iris.data
 k=3
-#
-k means=k means(n_clusters=k)
-#
-k means.fit(x)
-#
-centroids=k means.cluster_centers_
-#
-labels=k means.labels_
-#
+kmeans=KMeans(n_clusters=k)
+kmeans.fit(X)
+centroids=kmeans.cluster_centers_
+labels=kmeans.labels_
 plt.figure(figsize=(8,6))
-plt.scatter[x[:,0]:,1],c=labels,cmap='viridis')
-plt.scatter[centroids:,0],centroids[:,1],marker='x',s=200,c='red',label='Centroids')
+plt.scatter(X[:,0], X[:,1],c=labels,cmap='viridis')
+plt.scatter(centroids[:,0],centroids[:,1],marker='x',s=200,c='red',label='Centroids')
 plt.xlabel(iris.feature_names[0])
 plt.ylabel(iris.feature_names[1])
 plt.title('k-means clustering of Iris Dataset')
